6d22d20e5f32ecbf0b1bf736e5da7d8e6ee286fd
David Senkus <david.senkus@gmail.com>
2019-05-20 14:19:33 +0300

    catch unhandled promise rejections

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
 import * as config from 'config';
 import app from "./app";
 
+process.on("uncaughtException", (e): void => {
+    console.log(e);
+    process.exit(1);
+});
+  
+process.on("unhandledRejection", (e): void => {
+    console.log(e);
+    process.exit(1);
+});
+
 export const server = app.listen(config.get('app.port'), (): void => {
     console.log(`Server started on port ${config.get('app.port')}`);
 });
