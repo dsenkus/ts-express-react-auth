68d4fa5681b79ce36dcc1842e43ab5ba8c21c972
David Senkus <david.senkus@gmail.com>
2019-05-30 12:56:20 +0300

    change logo

--- client/src/components/AppUnauthenticated/AppUnauthenticated.tsx
+++ client/src/components/AppUnauthenticated/AppUnauthenticated.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
-// import LoginPage from '../../pages/LoginPage/LoginPage';
-import {
-  Route,
-  Switch
-} from 'react-router-dom';
-// import RegisterPage from '../../pages/RegisterPage/RegisterPage';
-// import ForgotPasswordPage from '../../pages/ForgotPasswordPage/ForgotPasswordPage';
-// import PageNotifications from '../PageNotifications/PageNotifications';
-import logo from '../../assets/logo_light.png';
 import bg from '../../assets/green_fibers.png';
-import './AppUnauthenticated.css';
-import LoginPage from '../../pages/LoginPage';
-import RegisterPage from '../../pages/RegisterPage';
 import ForgotPasswordPage from '../../pages/ForgotPasswordPage';
+import LoginPage from '../../pages/LoginPage';
+import logo from '../../assets/logo.png';
+import RegisterPage from '../../pages/RegisterPage';
+import { Link, Route, Switch } from 'react-router-dom';
+import './AppUnauthenticated.css';
 
 const headerStyles = {
   backgroundImage: `url(${bg})`
@@ -21,12 +14,10 @@ const headerStyles = {
 const AppUnauthenticated = () => {
   return (
     <div className="AppUnauthenticated">
-      {/* <PageNotifications /> */}
-
       <div className="auth-box flex flex-column center min-vh-100 justify-center ph3">
         <div className="shadow-40">
           <div className="auth-box-header cover w-140 tc pa4" style={headerStyles}>
-            <a href="/"><img src={logo} alt="Logo" style={{ height: '2.5rem' }}/></a>
+            <Link to="/"><img src={logo} alt="Logo" style={{ height: '2.5rem' }}/></Link>
           </div>
 
           <div className="auth-box-content w-100 bg-white pad">
